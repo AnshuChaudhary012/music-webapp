affcde03cb377bb6bacad4499d2b709423af8be3
Anshu Chaudhary <anshukohar09@gmail.com>
2025-07-23 22:26:14 +0530

    my the chick pause chage to play svg

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProcessBar = document.getElementById('myProcessBar');
+const play = document.getElementById('play');
 
 let songs = [
     { songName: "Julfa", filepath: "songs/1.mp3", coverpath: "covers/1.jpg" },
@@ -18,8 +19,13 @@ let songs = [
 masterPlay.addEventListener('click', () => {
     if (audioElement.paused || audioElement.currentTime <= 0) {
         audioElement.play();
+        masterPlay.src='svgs/play.svg';
+ // here the size of play svg is small so i give the size to this svg 
+        masterPlay.style.width = '50px';   
+        masterPlay.style.height = '50px';
     } else {
         audioElement.pause();
+        masterPlay.src='svgs/pause.svg';
     }
 });
 
